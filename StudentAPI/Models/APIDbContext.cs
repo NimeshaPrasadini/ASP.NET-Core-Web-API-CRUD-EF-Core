88b01048f97a07f0f28e5dee1a9e14d0b26ec1bd
Nimesha Prasadini <apnimeshap@gmail.com>
2023-09-19 10:49:29 +0530

    added 'StudentAPI' project

---
+++ StudentAPI/Models/APIDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace StudentAPI.Models
+{
+    public class APIDbContext : DbContext
+    {
+        public APIDbContext(DbContextOptions options) : base(options)
+        {}
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
